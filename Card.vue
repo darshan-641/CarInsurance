15e1feac9b1282b8e7ebe8546ad8849656d05307
Darshan Revgade <56465354+darshan-641@users.noreply.github.com>
2023-09-18 17:49:22 +0530

    Salil sathi code

---
+++ Card.vue
@@ -0,0 +1,69 @@
+  <template>
+    <div class="card-main-body">
+      <div class="img-body">
+        <img :src="img"  alt="this is image" />
+      </div>
+      <div class="info">
+        <h3 class="title">{{ title }}</h3>
+        <h4 class="views">{{views}} views</h4>
+        <span>{{ id}}</span>
+        <p class="description">{{ description.substring(0,170) }}...</p>
+        <button class="button" @click="$emit('sendBack',id)">Remove</button>
+      </div>
+    </div>
+  </template>
+
+  <script setup>
+      const props = defineProps({title:String, views:Number, description:String, img:String, id:String});
+  </script>
+
+<style scoped>
+
+.card-main-body{
+    display: flex;
+    align-items: flex-start;
+    justify-content:flex-start;
+    margin: 2vh 0 2vh 0;
+    padding: 5vh 15vw 0vh 15vw;
+}
+
+.info h3{
+  margin-top: 0px;
+}
+
+.info{
+    padding-left: 5vw;
+  
+}
+
+.title{
+    color: #f1f1f1;
+    font-size: 32px;
+    font-weight: 800;
+}
+
+.view, .description{
+    color: #aaa;
+}
+
+
+.img-body img{
+    height: 250px;
+}
+.button {
+  background-color: rgb(27, 26, 26);
+  border: 0.7px solid #ccc;
+  color: #ccc;
+  padding: 10px 20px;
+  cursor: pointer;
+  border-radius: 10px;
+}
+
+.button:hover {
+  background-color: #ccc;
+  transition: 0.09s ease-in;
+  color: rgb(27, 26, 26);
+}
+
+
+</style>
---
+++ Home.vue
@@ -0,0 +1,83 @@
+
+<script setup>
+import Navbar from "./components/Navbar.vue";
+import Card from './components/Card.vue';
+import Footer from "./components/Footer.vue";
+
+
+import {ref} from 'vue';
+import data1 from "./assets/data.json"
+
+let dataList = ref(data1)
+
+
+  const removeItem = (id) =>{
+  
+ 
+    for(var data in dataList.value){
+   
+      console.log("This is data.id " + dataList.value[data].id)
+   
+      
+      console.log("This is mf data " + dataList.value[data].isHidden)
+      if( dataList.value[data].id == id){
+
+
+        dataList.value[data].isHidden = true;
+      }
+    }
+  }
+
+</script>
+
+<template>
+  
+  <Navbar/>
+  <router-view/>
+  <div v-for="data in dataList" :key="data.id">
+    <Card :img="data.imgSrc" :title="data.title" :description="data.desc" v-show="!data.isHidden" :views="data.views" :id="data.id" @sendBack="(id)=>{
+      removeItem(id);
+    }" />
+   
+  </div>
+
+  
+  <Footer/>
+
+</template>
+
+
+
+<style scoped>
+
+
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap");
+
+body {
+  margin: 0px;
+  height: 100vh;
+  background-color: rgb(27, 26, 26);
+  color: rgb(236, 236, 236);
+  font-family: "Roboto", sans-serif;
+}
+
+
+
+h1 {
+
+  font-size: 4em;
+}
+.hello{
+    font-size: 2rem;
+    font-weight: bold;
+}
+.main-container {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  padding: 0 15% 0 15%;
+}
+</style>
